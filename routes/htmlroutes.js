2f3db645f30686066601065c4156d1c85938700e
Dylan Frank <drfrank22@gmail.com>
2021-01-25 23:29:06 -0500

    small change

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -7,7 +7,7 @@ router.get("/notes", function(req, res) {
 });
 
     // display index
-router.get("*", function(req, res) {
+router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
 
